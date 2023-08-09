32298f20a8aff9e7b95dfa7c4eb100d0c3ec9bbe
ARAMIN <aramin@ford.com>
2023-08-09 16:52:30 -0400

    fix init for bigquery components

--- components/bigquery-components/src/bigquery_components/__init__.py
+++ components/bigquery-components/src/bigquery_components/__init__.py
@@ -1,5 +1,9 @@
 from .bq_query_to_table import bq_query_to_table
 from .extract_bq_to_dataset import extract_bq_to_dataset
+from .ingest_features_gcs import ingest_features_gcs
+from .copy_bigquery_data_comp import copy_bigquery_data_comp
+from .evaluate_model import evaluate_model
+from .feature_engineering_comp import feature_engineering_comp
 
 
 __version__ = "0.0.1"
